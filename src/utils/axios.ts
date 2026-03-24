4045db006cd92f69dfede5fc6eea435ee0d313fc
Sivabalan-19 <pmsiva.1906@gmail.com>
2026-03-24 10:44:58 +0530

    env changed

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosHeaders, AxiosRequestConfig } from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3004/api";
+const API_BASE_URL = process.env.BASE_URL ?? "http://event-portal-server.vercel.app/api";
 const AUTH_TOKEN_KEY = "token";
 
 function getStoredToken(): string | null {
