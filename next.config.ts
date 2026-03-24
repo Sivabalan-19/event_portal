f63ef11f76c1f0f6a24ca9e80fc6c45f0e29b104
Sivabalan-19 <pmsiva.1906@gmail.com>
2026-03-24 11:10:51 +0530

    changed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+const API_BACKEND = process.env.BASE_URL ?? "https://event-portal-server.vercel.app/api";
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${API_BACKEND}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
